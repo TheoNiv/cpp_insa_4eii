5e15e1ec09b02ff92224b48b99a0b3fb973a4504
Théo Chatelier <tchateli@insa-rennes.fr>
2017-10-26 10:45:47 +0200

    Suppressed unnecessary members

--- TP3/MySquare.h
+++ TP3/MySquare.h
@@ -3,10 +3,6 @@
 class MySquare :
 	public MyRectangle
 {
-protected:
-	int dx_;
-	int dy_;
-
 public:
 	MySquare(const int& x, const int& y, const int& length, const bool& fill, const Color& color)
 		: MyRectangle::MyRectangle(x, y, length, length, fill, color) {};
